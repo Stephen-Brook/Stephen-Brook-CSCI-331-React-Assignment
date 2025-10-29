f203a7dbee972e4d7287dad2bc90b272f62d68d1
Stephen-Brook <stephenb0708@gmail.com>
2025-10-29 17:39:15 -0600

    minor updates

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,8 +6,6 @@ export default function AboutPage() {
       <h1>About / Repo</h1>
       <RepoBlurb
         repoUrl="https://github.com/Stephen-Brook/Stephen-Brook-CSCI-331-React-Assignment" 
-        topic="Vue Framework"
-        howShowcase="measurable performance gains using image optimization, route-level caching, and client/server components where appropriate"
       />
     </div>
   );
